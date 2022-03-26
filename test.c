c09a198c36db5249d4e09e48c5043cdbd99d49df
surya112k <kmsurya98@gmail.com>
2022-03-26 20:34:13 +0530

    First step done

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
+#include "test1.h"
+#include "test2.h"
 
 void fordelay(int j)
 {   int i,k;
@@ -7,32 +9,51 @@ void fordelay(int j)
          k=i;
 }
 
+void close(void)
+{
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tThis C Mini Project is developed by Surya.K.M");
+    for(int i=0;i<=6;i++)
+    {
+      fordelay(100000000);
+      printf(".");
+    }
+}
+
 int main()
 {
     char pass[20],password[20]="surya";
-    int i=0;
+    int i=0,a;
     int main_exit;
     printf("\n\n\n\n\t\tEnter the password to login:");
     scanf("%s",pass);
     if (strcmp(pass,password)==0)
-        {printf("\n\nPassword Match!\nLOADING");
-        for(i=0;i<=6;i++)
         {
+          printf("\n\nPassword Match!\nLOADING");
+          system("cls");
+          printf("Menu calling test1");
+          for(i=0;i<=6;i++)
+          {
             fordelay(100000000);
             printf(".");
-        }
-                system("cls");
-            //menu();
-            printf("Menu calling...");
-            fordelay(500000000);
+          }
+          system("cls");
+          a=menu();
+          switch(a)
+            {
+              case 1:new_acc();
+              break;
+            }
+          system("cls");
+          close();
         }
     else
-        {   printf("\n\nWrong password!!\a\a\a");
-            login_try:
-            printf("\nEnter 1 to try again and 0 to exit:");
-            scanf("%d",&main_exit);
-            if (main_exit==1)
-                    {
+        {   
+          printf("\n\nWrong password!!\a\a\a");
+          login_try:
+          printf("\nEnter 1 to try again and 0 to exit:");
+          scanf("%d",&main_exit);
+          if (main_exit==1)
+            {
 
                         system("cls");
                         main();
@@ -41,7 +62,7 @@ int main()
             else if (main_exit==0)
                     {
                     system("cls");
-                    //close();
+                    close();
                     printf("Working close");}
             else
                     {printf("\nInvalid!");
