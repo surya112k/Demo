da72e7814ad9ac898e735d70dea8d68209e55972
surya112k <kmsurya98@gmail.com>
2022-03-26 19:00:33 +0530

    Initial work done

--- test.c
+++ test.c
@@ -1,9 +1,54 @@
 #include <stdio.h>
+#include <windows.h>
+
+void fordelay(int j)
+{   int i,k;
+    for(i=0;i<j;i++)
+         k=i;
+}
 
 int main()
 {
-  printf("Hello World\n");
-  printf("Done\n");
-  printf("Perfect");
-  return 0;
+    char pass[20],password[20]="surya";
+    int i=0;
+    int main_exit;
+    printf("\n\n\n\n\t\tEnter the password to login:");
+    scanf("%s",pass);
+    if (strcmp(pass,password)==0)
+        {printf("\n\nPassword Match!\nLOADING");
+        for(i=0;i<=6;i++)
+        {
+            fordelay(100000000);
+            printf(".");
+        }
+                system("cls");
+            //menu();
+            printf("Menu calling...");
+            fordelay(500000000);
+        }
+    else
+        {   printf("\n\nWrong password!!\a\a\a");
+            login_try:
+            printf("\nEnter 1 to try again and 0 to exit:");
+            scanf("%d",&main_exit);
+            if (main_exit==1)
+                    {
+
+                        system("cls");
+                        main();
+                    }
+
+            else if (main_exit==0)
+                    {
+                    system("cls");
+                    //close();
+                    printf("Working close");}
+            else
+                    {printf("\nInvalid!");
+                    fordelay(1000000000);
+                    system("cls");
+                    goto login_try;}
+
+        }
+        return 0;
 }
