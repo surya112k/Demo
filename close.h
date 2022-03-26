cb875779e19cf1ad31fc2bcb82c1915e9649e907
surya112k <kmsurya98@gmail.com>
2022-03-26 21:27:41 +0530

    close file initializing

---
+++ close.h
@@ -0,0 +1,9 @@
+void close(void)
+{
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tThis C Mini Project is developed by Surya.K.M");
+    for(int i=0;i<=6;i++)
+    {
+      fordelay(100000000);
+      printf(".");
+    }
+}
