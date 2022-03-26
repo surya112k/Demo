976ed70b8f7c9961f7bfd3a54c15b66cc7c61c47
surya112k <kmsurya98@gmail.com>
2022-03-26 20:34:28 +0530

    first demo is done

---
+++ test1.h
@@ -0,0 +1,16 @@
+int menu()
+{   int choice,i,k,j =200000000;
+    system("cls");
+    system("color F0");
+    printf("\n\n\t\t\tCUSTOMER ACCOUNT BANKING MANAGEMENT SYSTEM");
+    printf("\n\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 WELCOME TO THE MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\n\n\t\t1.Create new account\n\t\t2.Update information of existing account\n\t\t3.For transactions\n\t\t4.Check the details of existing account\n\t\t5.Removing existing account\n\t\t6.View customer's list\n\t\t7.Exit\n\n\n\n\n\t\t Enter your choice:");
+    scanf("%d",&choice);
+    printf("\nThe choice is : %d",choice);
+    for(i=0;i< j ;i++)
+    {
+        k=i;
+    }
+    system("color 07");
+    return 1;
+}
