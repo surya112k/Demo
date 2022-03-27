b2096c67dfaaa1be659b2d3039b1e7abc2fda617
surya112k <kmsurya98@gmail.com>
2022-03-27 19:00:18 +0530

    headers file created for store headers

---
+++ headers.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+#include "vardeclare.h"
+#include "menu.h"
+#include "newacc.h"
+#include "edit.h"
+#include "transaction.h"
+#include "see.h"
+#include "erase.h"
+#include "view.h"
+#include "delay.h"
+#include "close.h"
