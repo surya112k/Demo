4f3ecf588c7aa0ec5ec2b5ded21a490dfe8f8248
surya112k <kmsurya98@gmail.com>
2022-03-27 19:02:00 +0530

    close file created

--- close.h
+++ close.h
@@ -1,11 +1,11 @@
-void close(void)
+void close()
 {
     system("cls");
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tThis C Mini Project is developed by Surya.K.M");
     printf("\n\t\t\t\tApplication closing");
     for(int i=0;i<=10;i++)
     {
-      fordelay(200000000);
+      delay(50000000);
       printf(".");
     }
 }
